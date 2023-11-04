51aaa530facda93f6eabadcbf4226511d0fcdb5d
Bilal Dieumegard <bilal.dieumegard@gmail.com>
2023-11-04 12:47:03 +0100

    Ajout et modification des crud Note et Commentaire Administrateur pour que la note soit comprise entre 0 et 5 et avec des sauts de 0.5

--- src/Controller/Admin/NoteCrudController.php
+++ src/Controller/Admin/NoteCrudController.php
@@ -5,7 +5,6 @@ namespace App\Controller\Admin;
 use App\Entity\Note;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
@@ -25,7 +24,11 @@ class NoteCrudController extends AbstractCrudController
             IdField::new('id')->hideOnForm(),
             AssociationField::new('anime'),
             AssociationField::new('user'),
-            NumberField::new('note'),
+            NumberField::new('note')
+                ->setFormTypeOptions([
+                'attr' => ['min' => 0, 'max' => 5, 'step' => 0.5],
+                'html5' => true,
+            ]),
             DateTimeField::new('date_creation'),
         ];
     }
