8a1b03197917d7bf6b5d98124aff78c5d30e72d7
Bilal Dieumegard <bilal.dieumegard@gmail.com>
2023-11-16 19:02:00 +0100

    Ajout de la navbar + css + js et du footer

---
+++ public/scripts/header.js
@@ -0,0 +1,15 @@
+let hamburger = document.querySelector('.nav .hamburger');
+let icon = hamburger.querySelector('.ham-icon')
+let right = document.querySelector('.nav .right')
+
+console.log(hamburger);
+hamburger.addEventListener('click',() =>{
+    console.log(hamburger);
+    if(right.classList.contains('active')){
+        right.classList.remove('active');
+        icon.classList.replace('fa-xmark','fa-bars');
+    }else{
+        icon.classList.replace('fa-bars','fa-xmark');
+        right.classList.add('active');
+    }
+})
