90af453be77a7471ef3797ec3f8ed860ae63a278
Nabil Ould Hamou <ouldhamounabil@gmail.com>
2023-11-19 13:22:39 +0100

    Suppression d'un script inutile et et modification en conséquence du twig qui l'importait.

--- public/scripts/stars.js
+++
@@ -1,9 +0,0 @@
-function halfStar(rating) {
-    const fifthStar = document.querySelector('.rating .star:nth-child(5)');
-
-    if (rating !== Math.floor(rating)) {
-        const decimalPart = rating - Math.floor(rating);
-        const widthPercentage = decimalPart * 100 + '%';
-        fifthStar.style.setProperty('--width', widthPercentage);
-    }
-}
