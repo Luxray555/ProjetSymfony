867158c6bee1785e40374ce61d1be70613afd8d8
Bilal Dieumegard <bilal.dieumegard@gmail.com>
2023-12-01 16:47:23 +0100

    Stars hover bug fixed + header profile image fixed

--- public/scripts/anime.js
+++ public/scripts/anime.js
@@ -17,16 +17,4 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    let stars = document.querySelectorAll('.form-stars .star-box');
-    for(let i = 0; i < stars.length; i++) {
-        stars[i].querySelector('.star-box input[type="radio"]').addEventListener('change', function () {
-            for(let j = 0; j < stars.length; j++) {
-                stars[j].classList.remove('active');
-            }
-            for(let j = 0; j < i+1; j++) {
-                stars[j].classList.add('active');
-            }
-        });
-    }
-
 });
