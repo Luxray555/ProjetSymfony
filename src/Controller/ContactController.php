e47fca5657037776863ddfa4dbdc61eb462a9244
Nabil Ould Hamou <ouldhamounabil@gmail.com>
2023-12-08 16:42:54 +0100

    Modification du README Fix de différent bugs, modification du readme et màj du domaine.

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,16 +4,19 @@ namespace App\Controller;
 
 use App\Entity\Contact;
 use App\Form\ContactType;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mime\Email;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $entityManager, UserRepository $userRepository, MailerInterface $mailer): Response
     {
         $contact = new Contact();
         $form = $this->createForm(ContactType::class, $contact);
@@ -25,6 +28,18 @@ class ContactController extends AbstractController
 
             $contact = new Contact();
             $form = $this->createForm(ContactType::class, $contact);
+
+            $admins = $userRepository->findAdmins();
+
+            foreach ($admins as $admin) {
+                $email = (new Email())
+                    ->from('contact@animeworld.world')
+                    ->to($admin->getEmail())
+                    ->subject('Une nouvelle demande de contact a été laissé.')
+                    ->text('Afin de voir la demande et d\'y répondre merci de vous rendre sur le panel admin.');
+
+                $mailer->send($email);
+            }
 
             return $this->render('contact/new.html.twig', [
                 'form' => $form,
