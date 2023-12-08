b4e38eb9f6331897b5c374cabb7acc674dc7096b
Bilal Dieumegard <bilal.dieumegard@gmail.com>
2023-12-08 17:32:59 +0100

    Fix js filtre de recherche anime

--- public/scripts/choicelist.js
+++ public/scripts/choicelist.js
@@ -1,5 +1,15 @@
 let choiceBox = document.querySelectorAll('.form-search .choice-list');
 
+document.addEventListener('click', function () {
+    for (let i = 0; i < choiceBox.length; i++) {
+        if (choiceBox[i].querySelector('.choice').contains(event.target)) {
+            choiceBox[i].classList.add('active');
+        } else if (choiceBox[i].querySelector('.choice-box').contains(event.target)) {
+        } else {
+            choiceBox[i].classList.remove('active');
+        }
+    }
+});
 document.addEventListener('DOMContentLoaded', function () {
     let list = {};
     for (let i = 0; i < choiceBox.length; i++) {
--- public/scripts/searchAnime.js
+++ public/scripts/searchAnime.js
@@ -5,16 +5,6 @@ filterBtn.addEventListener('click', function () {
     filter.classList.toggle('active');
     filterBtn.classList.toggle('active');
 });
-document.addEventListener('click', function () {
-    for (let i = 0; i < choiceBox.length; i++) {
-        if (choiceBox[i].querySelector('.choice').contains(event.target)) {
-            choiceBox[i].classList.add('active');
-        } else if (choiceBox[i].querySelector('.choice-box').contains(event.target)) {
-        } else {
-            choiceBox[i].classList.remove('active');
-        }
-    }
-});
 
 
 
