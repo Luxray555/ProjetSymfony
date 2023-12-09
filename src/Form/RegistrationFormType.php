a3868bee61049cf06879bdc90b9ac9fbe0d998e8
Bilal Dieumegard <bilal.dieumegard@gmail.com>
2023-12-09 18:39:16 +0100

    Fix email dans l'inscription

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,7 +5,9 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,14 +19,14 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', null, [
+            ->add('email', EmailType::class, [
                 'label' => 'Adresse email',
                 'attr' => [
                     'autocomplete' => 'email',
                     'class' => 'form-control'
                 ]
             ])
-            ->add('username', null, [
+            ->add('username', TextType::class, [
                 'label' => 'Pseudo',
                 'attr' => [
                     'autocomplete' => 'username',
